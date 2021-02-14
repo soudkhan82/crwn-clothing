c4272d02cef7566b019536b54c7204de53c21710
Saud Arshad Khan <saud.khan@cmpak.local>
2021-02-14 14:07:04 +0500

    update our menu item to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
+import {Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+
+const TopicsList=(props)=>{
+  console.log(props);
+  return( 
+  <div>
+    
+    <h1>TopicsList</h1>
+  </div>);
+}
+
+
+const TopicDetail=(props)=>{
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic Detail Page : {props.match.params.topicId}</h1>
+    </div>
+  );
+}
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Route exact path='/'component={HomePage}></Route>
+      <Route exact path='/topics' component={TopicsList}></Route>
+      <Route path='/topics/:topicId'component={TopicDetail}></Route>
+     
+      
     </div>
   );
 }
