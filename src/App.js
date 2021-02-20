d0ac5f4644cf64ca170afe404562ac9761971ca9
Saud Arshad Khan <saud.khan@cmpak.local>
2021-02-20 21:32:44 +0500

    updating our app with Shop page and collection preview

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import {Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 
-const TopicsList=(props)=>{
-  console.log(props);
-  return( 
-  <div>
-    
-    <h1>TopicsList</h1>
-  </div>);
-}
 
-
-const TopicDetail=(props)=>{
-  console.log(props);
-  return (
-    <div>
-      <h1>Topic Detail Page : {props.match.params.topicId}</h1>
-    </div>
-  );
-}
 function App() {
   return (
     <div>
       <Route exact path='/'component={HomePage}></Route>
-      <Route exact path='/topics' component={TopicsList}></Route>
-      <Route path='/topics/:topicId'component={TopicDetail}></Route>
+      <Route path='/shop'  component={ShopPage}></Route>
      
       
     </div>
